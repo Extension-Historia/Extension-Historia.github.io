d66f9fbfad3bcc91d93f2d0f545d096c33ad4b2e
Lina Lerch <64536935+linalerch@users.noreply.github.com>
2022-12-09 10:37:43 +0100

    Update popup.js

--- online-version/popup.js
+++ online-version/popup.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
     $('#example').DataTable({
         paging: false,
         ordering: false,
-        info: false,
+        info: true,
   			"language": {
     					"search": "",
               "searchPlaceholder": "Search..."
   			},
+        "sDom": '<"top"<"actions">lfpi<"clear">><"clear">rt<"bottom">'
 
     });
 });
