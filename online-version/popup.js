61e463ab6a2301ca03873580cdefb1bf083a1a66
Lina Lerch <64536935+linalerch@users.noreply.github.com>
2022-12-16 09:04:14 +0100

    Update popup.js

--- online-version/popup.js
+++ online-version/popup.js
@@ -6,10 +6,8 @@ $(document).ready(function () {
   			"language": {
     					"search": "",
                         "searchPlaceholder": "Search...",
-                        "info": "Showing" _TOTAL_ "entries",
-
+                        "info": "Showing _END_ of _TOTAL_ entries"
   			},
-
         "sDom": '<"top"<"actions">lfpi<"clear">><"clear">rt<"bottom">'
 
     });
