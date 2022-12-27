81915901c2f413f7ee57f0a96e6af76aa8e1ee04
Lina Lerch <64536935+linalerch@users.noreply.github.com>
2022-12-27 11:51:59 +0100

    Add files via upload

---
+++ test-version2/popup.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $('#example').DataTable({
+        paging: false,
+        ordering: false,
+        info: true,
+        "language": {
+                        "info": "Showing _TOTAL_ from _MAX_ entries",
+                        "infoFiltered": "",
+                        "infoEmpty": ""
+  			},
+
+        dom: 'irtp',
+
+    });
+
+oTable = $('#example').DataTable();
+$('#custom_searchbox').keyup(function(){
+      oTable.search($(this).val()).draw() ;
+})
+
+});
+
+$(document).ready( function () {
+
+  var table = $('#example').DataTable();
+
+ $('input:checkbox').on('change', function () {
+   //build a filter string with an or(|) condition
+   var keywords = $('input:checkbox[name="key"]:checked').map(function() {
+     return this.value;
+   }).get().join('|');
+
+
+   //now filter in column 2, with no regex, no smart filtering, not case sensitive
+   table.column(0).search(keywords, true, false, false).draw(false);
+
+ });
+
+} );
