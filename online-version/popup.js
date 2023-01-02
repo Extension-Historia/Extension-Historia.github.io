0bed224f0a1c47976f3b8ba904d752640920eab4
Lina Lerch <64536935+linalerch@users.noreply.github.com>
2023-01-02 12:02:03 +0100

    Update popup.js

--- online-version/popup.js
+++ online-version/popup.js
@@ -1,16 +1,39 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $('#example').DataTable({
         paging: false,
         ordering: false,
         info: true,
-  			"language": {
-    					"search": "",
-                        "searchPlaceholder": "Search...",
-                        "info": "&nbsp;&nbsp;Showing _TOTAL_ from _MAX_ entries",
+        "language": {
+                        "info": "Showing _TOTAL_ from _MAX_ entries",
                         "infoFiltered": "",
                         "infoEmpty": ""
   			},
-        "sDom": '<"top"<"actions">lfpi<"clear">><"clear">rt<"bottom">'
+
+        dom: 'irtp',
 
     });
+
+oTable = $('#example').DataTable();
+$('#custom_searchbox').keyup(function(){
+      oTable.search($(this).val()).draw() ;
+})
+
 });
+
+$(document).ready( function () {
+
+  var table = $('#example').DataTable();
+
+ $('input:checkbox').on('change', function () {
+   //build a filter string with an or(|) condition
+   var keywords = $('input:checkbox[name="key"]:checked').map(function() {
+     return this.value;
+   }).get().join('|');
+
+
+   //now filter in column 2, with no regex, no smart filtering, not case sensitive
+   table.column(0).search(keywords, true, false, false).draw(false);
+
+ });
+
+} );
